61f3656f0d1e898de6cda4612c0550917b55ca43
Aidan N <anolan.webdev@gmail.com>
2019-04-24 11:19:05 +0100

    add max min temperatures and conversion from kelvin to Celsius

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import CityWeather from './CityWeather'
 
 class App extends Component {
   render() {
+    const today = new Date().toLocaleDateString("en-GB");
     return (
       <div className="App">
+        <h1>Weather on {today}</h1>
         <CityWeather id="5128638" />
         <CityWeather id="2643743" />
-        <CityWeather id="6618607" />
+        <CityWeather id="3614789" />
       </div>
     );
   }
--- src/CityWeather.js
+++ src/CityWeather.js
@@ -6,32 +6,45 @@ class CityWeather extends Component {
         this.state = {
           weather:[],
           icon: "",
-          city: ""
+          city: "",
+          tempMin: "",
+          tempMax: ""
         }
-      }
+        this.handleClick = this.handleClick.bind(this);
+    }
     
-      componentDidMount(){
-        console.log(this.props.id);
-        fetch("http://api.openweathermap.org/data/2.5/weather?id=" + this.props.id + "&appid=f04826057701fdaf14bbacf1ee8c24cb")
+    componentDidMount(){
+        fetch("https://api.openweathermap.org/data/2.5/weather?id=" + this.props.id + "&appid=f04826057701fdaf14bbacf1ee8c24cb")
           .then(response => response.json())
           .then(response => {
               const weather = response.weather[0].main;
               const icon = response.weather[0].icon;
               const city = response.name;
+              const tempMin = Math.round(response.main.temp_min - 273.15);
+              const tempMax = Math.round(response.main.temp_max - 273.15);
               this.setState({
                 weather: weather,
-                icon: "http://openweathermap.org/img/w/" + icon + ".png",
-                city: city
+                icon: "https://openweathermap.org/img/w/" + icon + ".png",
+                city: city,
+                tempMin: tempMin,
+                tempMax: tempMax
               })
           })
-      }
+    }
+
+    handleClick(){
+        console.log("worked");
+    }
 
     render(){
         return(
-            <div>
+            <div className="container">
                 <h1>{this.state.city}</h1>
                 <h2>{this.state.weather}</h2>
                 <img className="icon" src={this.state.icon} alt={this.state.weather}/>
+                <h3>Min: {this.state.tempMin}°C</h3><h3>Max: {this.state.tempMax}°C</h3>
+                <br />
+                <button onClick={this.handleClick}>Next City</button>
             </div> 
         )
     }
